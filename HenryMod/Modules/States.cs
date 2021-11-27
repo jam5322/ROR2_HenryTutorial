cf9256bf819bda15954e5852bd94b39cef6712f7
Jonah Micek <54957570+jam5322@users.noreply.github.com>
2021-11-26 22:24:53 -0500

    Initial commit

---
+++ HenryMod/Modules/States.cs
@@ -0,0 +1,24 @@
+﻿using HenryMod.SkillStates;
+using HenryMod.SkillStates.BaseStates;
+using System.Collections.Generic;
+using System;
+
+namespace HenryMod.Modules
+{
+    public static class States
+    {
+        internal static List<Type> entityStates = new List<Type>();
+
+        internal static void RegisterStates()
+        {
+            entityStates.Add(typeof(BaseMeleeAttack));
+            entityStates.Add(typeof(SlashCombo));
+
+            entityStates.Add(typeof(Shoot));
+
+            entityStates.Add(typeof(Roll));
+
+            entityStates.Add(typeof(ThrowBomb));
+        }
+    }
+}
